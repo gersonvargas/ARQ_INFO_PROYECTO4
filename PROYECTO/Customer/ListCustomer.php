f67248140fb6955cdac9707de6b164d8c977b026
Gerson Vargas <gersonvargasgalvez@gmail.com>
2018-11-03 09:46:21 -0600

    bootstrap

--- PROYECTO/Customer/ListCustomer.php
+++ PROYECTO/Customer/ListCustomer.php
@@ -25,16 +25,15 @@ if (isset($_SESSION['error_msg'])) {
 <!doctype html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport"
-              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Customers</title>
-        <link href="../Bootstrap4/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <script src="../Bootstrap4/js/bootstrap.min.js" type="text/javascript"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="theme-color" content="#000000">
 
-        <link rel="shortcut icon" type="image/png" href="../images/mp4.png"/>
-        <link href="../styles.css" rel="stylesheet" type="text/css"/>
+        <link rel="manifest" href="%PUBLIC_URL%/manifest.json">
+        <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
+        <!--Importacion del css-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <title>Customer Phone Bill</title>
     </head>
     <body>
 
@@ -131,6 +130,10 @@ if (isset($_SESSION['error_msg'])) {
                 <span class="text-muted">Place sticky footer content here.</span>
             </div>
         </footer>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+        <script src="https://unpkg.com/ionicons@4.4.2/dist/ionicons.js"></script>
     </body>
 </html>
 
