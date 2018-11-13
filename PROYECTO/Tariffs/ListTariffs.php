c56433a5652db90ec12b706c2e9f585f3f24144f
Gerson Vargas <gersonvargasgalvez@gmail.com>
2018-11-13 16:04:21 -0600

    footer

--- PROYECTO/Tariffs/ListTariffs.php
+++ PROYECTO/Tariffs/ListTariffs.php
@@ -123,45 +123,17 @@ if (isset($_SESSION['error_msg'])) {
 
         <footer class="footer">
             <div class="container">
-             
-            <div class="topnav">
-                <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                    <a class="navbar-brand" href="#">Customer Phone Bill</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                <div class="">
+                    <nav class="navbar navbar-expand-md navbar-dark bg-light">
+                        <div class="collapse navbar-collapse text-center" id="">
+                            
+                            <p class="text-center"><b> Gerson Vargas G & Osvaldo Aguero &nbsp;|&nbsp;</b></p></br>
+                            <p class="text-center"><b> Arquitectura de Información, Proyecto  4&nbsp;</b></p>
+                            <p class="text-center"><b>|&nbsp;UNA 2018.</b></p></br>
+                        </div>
+                    </nav>
+                </div>
 
-                    <div class="collapse navbar-collapse" id="navbarsExample04">
-                        <ul class="navbar-nav mr-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="../Index.php">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../Customer/ListCustomer.php">Customers</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../Address/ListAddress.php">Addresses</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../Tariffs/ListTariffs.php">Tariffs</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../PhoneNumber/ListPhoneNumbers.php">Phone Numbers</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../PhoneCalls/ListPhoneCalls.php">Phone Calls</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="../Bill/ListBill.php">Bills</a>
-                            </li>
-                        </ul>
-                        <form class="form-inline my-2 my-md-0">
-                            <input class="form-control" type="text" placeholder="Search">
-                        </form>
-                    </div>
-                </nav>
-            </div>
-        
             </div>
         </footer>  
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
